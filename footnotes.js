62ff66318a11e014b2feb6751229fe83a074ecae
Alex Becker <acbecker@uchicago.edu>
2016-12-25 16:41:47 -0800

    add js/css for inline footnotes

---
+++ footnotes.js
@@ -0,0 +1,40 @@
+function inlineFootnote(footnote) {
+    var ref = document.getElementById("a-" + footnote.id);
+    var inline = document.createElement("div");
+    for (var i=0; i<footnote.children.length; i++) {
+        var child = footnote.children[i];
+        if (child.tagName == "A" && child.getAttribute("href") == "#" + ref.id) {
+            continue;
+        }
+        inline.appendChild(child);
+    }
+    inline.className = "inline-footnote";
+    inline.style.display = "none";
+    ref.parentNode.parentNode.insertBefore(inline, ref.parentNode.nextSibling);
+    ref.origIndex = ref.textContent;
+    ref.expanded = false;
+    ref.addEventListener("click", function (e) {
+        e.preventDefault();
+        if (ref.expanded) {
+            inline.style.display = "none";
+            ref.textContent = ref.origIndex;
+        } else {
+            inline.style.display = "block";
+            ref.textContent = "X";
+        }
+        ref.expanded = !ref.expanded;
+    });
+}
+
+function inlineFootnotes() {
+    var footnoteLists = document.getElementsByClassName("footnotes");
+    while (footnoteLists.length > 0) {
+        var footnotes = footnoteLists[0].children;
+        for (var j=0; j<footnotes.length; j++) {
+            inlineFootnote(footnotes[j]);
+        }
+        footnoteLists[0].remove();
+    }
+}
+
+inlineFootnotes()
